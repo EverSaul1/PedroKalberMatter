d786f810a7f58fc18103d20493c2fb1a82708f40
WillianHuamanttupaMar <willian.hm7@gmail.com>
2018-10-31 17:55:44 -0500

    usuario

--- app/src/main/java/pe/edu/upeu/pedrokasistenciaqr/MainActivity.java
+++ app/src/main/java/pe/edu/upeu/pedrokasistenciaqr/MainActivity.java
@@ -2,14 +2,20 @@ package pe.edu.upeu.pedrokasistenciaqr;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import OpenHelper.DBCon;
+import pe.edu.upeu.dblibrary.ExternalSQLiteOpenHelper;
 
 public class MainActivity extends AppCompatActivity {
     Context context;
     Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText txtusu=(EditText)findViewById(R.id.editText);
+                EditText txtcla=(EditText)findViewById(R.id.editText3);
+
+            }
+        });
+
 
     }
 }
